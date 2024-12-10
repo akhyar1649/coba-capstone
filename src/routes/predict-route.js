077e9e74d52943660c288e21136571b5db027e65
Akhyar1649 <akhyaramin5@gmail.com>
2024-12-10 14:45:51 +0700

    Fix some code

--- src/routes/predict-route.js
+++ src/routes/predict-route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { predictForm } = require("../handlers/predict-handler.js");
 
-router.get("/form", predictForm);
+router.post("/form", predictForm);
 // router.get("/image", predictImage);
 
 module.exports = router;
