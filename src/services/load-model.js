cfa8817a4a78b9be4ed4f995527ddee26c5e6388
Akhyar1649 <akhyaramin5@gmail.com>
2024-12-10 15:10:57 +0700

    Fix some code

--- src/services/load-model.js
+++ src/services/load-model.js
@@ -1,7 +1,21 @@
-const tf = require("@tensorflow/tfjs-node");
+const tf = require('@tensorflow/tfjs-node');
+let model;
 
-async function loadModel(model) {
-  return tf.loadLayersModel(model);
-}
+const loadModel = async () => {
+    const loadedModel = await tf.loadLayersModel(process.env.MODEL_FORM);
+    if (!loadedModel.inputs || loadedModel.inputs.length === 0) {
+        const inputLayer = tf.input({ shape: [9] });
+        const output = loadedModel.apply(inputLayer);
+        model = tf.model({ inputs: inputLayer, outputs: output });
+    } else {
+        model = loadedModel;
+    }
+};
 
-module.exports = loadModel;
+const predict = (inputData) => {
+    if (!model) throw new Error('Model is not loaded.');
+    const inputTensor = tf.tensor2d([inputData], [1, 9]);
+    return model.predict(inputTensor).arraySync();
+};
+
+module.exports = { loadModel, predict };
