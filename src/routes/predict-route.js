8ee95adb9abeeca4e63615cb5ad7301d854de5d6
Akhyar1649 <akhyaramin5@gmail.com>
2024-12-10 18:46:00 +0700

    Fix some code

--- src/routes/predict-route.js
+++ src/routes/predict-route.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const { predictForm } = require("../handlers/predict-handler.js");
+const loadModel = require("../services/load-model.js");
 
-router.post("/form", predictForm);
+router.post("/form", loadModel);
 // router.get("/image", predictImage);
 
 module.exports = router;
--- src/services/load-model.js
+++ src/services/load-model.js
@@ -1,7 +1,8 @@
 const tf = require("@tensorflow/tfjs-node");
 
-async function loadModel() {
-  return tf.loadLayersModel("https://storage.googleapis.com/coba-capstone-model/model/model-form/model.json");
+async function loadModel(req, res) {
+  const model = tf.loadLayersModel("https://storage.googleapis.com/coba-capstone-model/model/model-form/model.json");
+  return res.json({ model: model})
 }
 
 module.exports = loadModel;
