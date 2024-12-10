ff224598d5a01c15f38bfc96826f4a6b1a685bbc
Akhyar1649 <akhyaramin5@gmail.com>
2024-12-10 18:12:36 +0700

    Fix some code

--- src/handlers/predict-handler.js
+++ src/handlers/predict-handler.js
@@ -5,34 +5,8 @@ async function predictForm(req, res) {
   try {
     // Load model
     const model = await loadModel(process.env.MODEL_FORM);
-    if (!model) {
-      return res.status(500).send({ error: "Model belum dimuat" });
-    }
-
-    // Ambil data input dari body request
-    const { inputData } = req.body;
-
-    // Ambil input shape dari model
-    const inputShape = model.inputs[0].shape.slice(1);
-    console.log("Model Input Shape:", inputShape);
-
-    // Validasi input data
-    if (!Array.isArray(inputData) || inputData.length !== inputShape[0]) {
-      return res.status(400).send({ error: "Invalid input data shape" });
-    }
-
-    // Konversi ke tensor dengan dimensi batch
-    const inputTensor = tf.tensor2d([inputData]); // Dimensi [1, inputShape[0]]
-    console.log("Input Tensor Shape:", inputTensor.shape);
-
-    // Lakukan prediksi
-    const prediction = model.predict(inputTensor);
-    const result = prediction.arraySync()[0]; // Konversi hasil prediksi ke array
-
-    // Kirim hasil prediksi ke client
-    res.json({ prediction: result });
   } catch (error) {
-    console.error("Error during prediction:", error);
+    console.error("Model:", error);
     res.status(500).send({ error: "Internal server error" });
   }
 }
