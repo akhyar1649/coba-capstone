718b65659a21f44cce10a14c95ed17c7ab4ec367
Akhyar1649 <akhyaramin5@gmail.com>
2024-12-10 20:04:45 +0700

    Fix some code

--- src/services/load-model.js
+++ src/services/load-model.js
@@ -1,8 +1,9 @@
 const tf = require("@tensorflow/tfjs-node");
 
 async function loadModel(req, res) {
-  const model = tf.loadLayersModel("https://storage.googleapis.com/coba-capstone-model/model/model-form/model.json");
-  return res.json({ model: model})
+  const model = await tf.loadLayersModel("https://storage.googleapis.com/coba-capstone-model/model/model-form/model.json");
+  console.log(model);
+  return res.json({ message: model})
 }
 
 module.exports = loadModel;
